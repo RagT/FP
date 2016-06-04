c1868f33d437dc9e14bcb3e799d437a517806bfe
RagT <raghutir@gmail.com>
2016-06-04 04:28:22 -0700

    Added FileSystem Classes

---
+++ Java/ThreadOS/SuperBlock.java
@@ -0,0 +1,15 @@
+/**
+ * Raghu Tirumala
+ * CSS 430 Final Project
+ *
+ * Superblock.java
+ */
+public class SuperBlock {
+    public int totalBlocks; // the number of disk blocks
+    public int totalInodes; // the number of inodes
+    public int freeList;    // the block number of the free list's head
+
+    public SuperBlock( int diskSize ) {
+
+    }
+}
---
+++ Java/ThreadOS/SysLib.java
@@ -0,0 +1,11 @@
+/**
+ * Raghu Tirumala
+ * CSS 430 Final Project
+ *
+ * Implementation of necessary methods for file system.
+ */
+public class SysLib {
+    int format( int files ) {
+
+    }
+}
