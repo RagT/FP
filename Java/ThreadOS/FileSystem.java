1c5f204eedba6e868a509d5c12b07f8672ddef0b
RagT <raghutir@gmail.com>
2016-06-05 23:34:49 -0700

    Updated FileSystem Class

--- Java/ThreadOS/FileSystem.java
+++ Java/ThreadOS/FileSystem.java
@@ -80,57 +80,77 @@ public class FileSystem {
         if(fte.mode.equals("w") || fte.mode.equals("a")) {
             return -1;
         }
-        int size  = buffer.length;   //set total size of data to read
-        int rBuffer = 0;            //track data read
-        int rError = -1;            //track error on read
-        int blockSize = 512;        //set block size
-        int itrSize = 0;            //track how much is left to read
+        int buffSize  = buffer.length;   //size of data to read
+        int rBuff = 0;
+        int blockSize = 512;        //set block size to 512
+        int itr = 0;
 
         synchronized(fte) {
-            while (fte.seekPtr < fsize(fte) && (size > 0)) {
+            while (fte.seekPtr < fsize(fte) && (buffSize > 0)) {
                 int currentBlock = fte.inode.findTargetBlock(fte.seekPtr);
-                if (currentBlock == rError) {
+                if (currentBlock == -1) {
                     break;
                 }
                 byte[] data = new byte[blockSize];
                 SysLib.rawread(currentBlock, data);
 
                 int dataOffset = fte.seekPtr % blockSize;
-                int blocksLeft = blockSize - itrSize;
+                int blocksRemaining = blockSize - itr;
                 int fileLeft = fsize(fte) - fte.seekPtr;
 
-                if (blocksLeft < fileLeft) {
-                    itrSize = blocksLeft;
+                if (blocksRemaining < fileLeft) {
+                    itr = blocksRemaining;
                 } else {
-                    itrSize = fileLeft;
+                    itr = fileLeft;
                 }
-                if (itrSize > size) {
-                    itrSize = size;
+                if (itr > buffSize) {
+                    itr = buffSize;
                 }
 
-                System.arraycopy(data, dataOffset, buffer, rBuffer, itrSize);
-                rBuffer += itrSize;
-                fte.seekPtr += itrSize;
-                size -= itrSize;
+                System.arraycopy(data, dataOffset, buffer, rBuff, itr);
+                rBuff += itr;
+                fte.seekPtr += itr;
+                buffSize -= itr;
             }
-            return rBuffer;
+            return rBuff;
         }
     }
 
+    //Write to file in the FilTableEntry from the data passed in
     public int write(FileTableEntry fte, byte[] data) {
+        //Check if fte is in correct mode to write to
+        if(fte.mode.equals("r") || fte == null) {
+            return -1;
+        }
+    }
 
+    public synchronized int seek(FileTableEntry fte, int offset, int loc){
+        if(loc < 1){
+            fte.seekPtr = 0 + offset;
+        }else if(loc == 1){
+            fte.seekPtr += offset;
+        }else if(loc == 2){
+            fte.seekPtr = fte.inode.length + offset;
+        }else{
+            fte.seekPtr = fte.inode.length;
+        }
+        if (fte.seekPtr > fte.inode.length) {
+            fte.seekPtr = fte.inode.length;
+        }
+        if (fte.seekPtr < 0) {
+            fte.seekPtr = 0;
+        }
+        return fte.seekPtr;
     }
 
     private boolean deallocBlocks(FileTableEntry fileTableEntry){
         short invalid = -1;
-        if (fileTableEntry.inode.count != 1)
-        {
+        if (fileTableEntry.inode.count != 1) {
             SysLib.cerr("Null Pointer");
             return false;
         }
 
-        for (short blockId = 0; blockId < fileTableEntry.inode.directSize; blockId++)
-        {
+        for (short blockId = 0; blockId < fileTableEntry.inode.directSize; blockId++) {
             if (fileTableEntry.inode.direct[blockId] != invalid)
             {
                 superBlock.returnBlock(blockId);
@@ -140,8 +160,7 @@ public class FileSystem {
 
         byte [] data = fileTableEntry.inode.freeIndirectBlock();
 
-        if (data != null)
-        {
+        if (data != null) {
             short blockId;
             while((blockId = SysLib.bytes2short(data, 0)) != invalid)
             {
