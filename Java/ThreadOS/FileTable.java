e304f91eb22a6727fc7c2274d9aed25b25783475
RagT <raghutir@gmail.com>
2016-06-08 03:58:49 -0700

    Update

--- Java/ThreadOS/FileTable.java
+++ Java/ThreadOS/FileTable.java
@@ -13,58 +13,126 @@ public class FileTable {
     }                             // from the file system
 
     // major public methods
-
-    // allocate a new file (structure) table entry for this file name
-    // allocate/retrieve and register the corresponding inode using dir
-    // increment this inode's count
-    // immediately write back this inode to the disk
-    // return a reference to this file (structure) table entry
-    public synchronized FileTableEntry falloc(String filename, String mode) {
-        short entryMode = getEntryMode(mode);
-        if(entryMode < 0) { //Invalid mode
-            return null;
-        }
-        short iNumber;
-        Inode fileNode = null;
-        FileTableEntry fte = null;
+    public synchronized FileTableEntry falloc( String filename, String mode ) {
+        short iNumber = -1; // inode number
+        Inode inode = null; // holds inode
 
         while (true) {
-            if(filename.equals("/")) {
-                iNumber = 0;
-            } else {
-                iNumber = dir.namei(filename);
-            }
-            if (iNumber < 0) {
-                if (entryMode == 0) {
-                    return null;
+            // get the inumber form the inode for given file name
+            iNumber = (filename.equals("/") ? (short) 0 : dir.namei(filename));
+
+            // if the inode for the given file exist
+            if (iNumber >= 0) {
+                inode = new Inode(iNumber);
+
+                // if the file is requesting ofr reading
+                if (mode.equals("r")) {
+
+                    // and its flag is read or used or unused
+                    // (nobody has read or written to that file)
+                    if (inode.flag == 2
+                            || inode.flag == 1
+                            || inode.flag == 0) {
+
+                        // change the flag of the node to read and break
+                        inode.flag = 2;
+                        break;
+
+                        // if the file is already written by someone, wait until finish
+                    } else if (inode.flag == 3) {
+                        try {
+                            wait();
+                        } catch (InterruptedException e) { }
+                    }
+
+                    // if the file is requested for writing or writing/riding or append
+                } else {
+
+                    // and the flag of that file is used, change the flag to write
+                    if (inode.flag == 1 || inode.flag == 0) {
+                        inode.flag = 3;
+                        break;
+
+                        // if the flag is read or write, wait until they finish
+                    } else {
+                        try {
+                            wait();
+                        } catch (InterruptedException e) { }
+                    }
                 }
-                if ((iNumber = dir.ialloc(filename)) < 0) {
-                    return null;
-                }
-                fileNode = new Inode();
+
+                // if the node for the given file does not exist,
+                // create a new inode for that file, use the alloc function from
+                // directory to get the inumber
+            } else if (!mode.equals("r")) {
+                iNumber = dir.ialloc(filename);
+                inode = new Inode(iNumber);
+                inode.flag = 3;
                 break;
-            }
-            fileNode = new Inode(iNumber);
-            if (fileNode.flag == 4) {
+
+            } else {
                 return null;
             }
-            if (fileNode.flag == 0 || fileNode.flag == 1) {
-                break;
-            }
-            if (entryMode == 0 && fileNode.flag == 0) {
-                break;
-            }
-            try {
-                wait();
-            } catch (InterruptedException e) {
-            }
         }
-        fileNode.count++;
-        fileNode.toDisk(iNumber);
-        fte = new FileTableEntry(fileNode, iNumber, mode);
-        table.add(fte);
-        return fte;
+
+        inode.count++;  // increse the number of users
+        inode.toDisk(iNumber);
+        // create new file table entry and add it to the file table
+        FileTableEntry entry = new FileTableEntry(inode, iNumber, mode);
+        table.addElement(entry);
+        return entry;
     }
+//    // allocate a new file (structure) table entry for this file name
+//    // allocate/retrieve and register the corresponding inode using dir
+//    // increment this inode's count
+//    // immediately write back this inode to the disk
+//    // return a reference to this file (structure) table entry
+//    public synchronized FileTableEntry falloc(String filename, String mode) {
+//        short entryMode = getEntryMode(mode);
+//        if(entryMode < 0) { //Invalid mode
+//            return null;
+//        }
+//        short iNumber;
+//        Inode fileNode = null;
+//        FileTableEntry fte = null;
+//
+//        while (true) {
+//            if(filename.equals("/")) {
+//                iNumber = 0;
+//            } else {
+//                iNumber = dir.namei(filename);
+//            }
+//            if (iNumber < 0) {
+//                if (entryMode == 0) {
+//                    return null;
+//                }
+//                if ((iNumber = dir.ialloc(filename)) < 0) {
+//                    return null;
+//                }
+//                fileNode = new Inode();
+//                break;
+//            }
+//            fileNode = new Inode(iNumber);
+//            if (fileNode.flag == 4) {
+//                return null;
+//            }
+//            if (fileNode.flag == 0 || fileNode.flag == 1) {
+//                break;
+//            }
+//            if (entryMode == 0 && fileNode.flag == 0) {
+//                break;
+//            }
+//            try {
+//                wait();
+//            } catch (InterruptedException e) {
+//            }
+//        }
+//        fileNode.count++;
+//        fileNode.toDisk(iNumber);
+//        fte = new FileTableEntry(fileNode, iNumber, mode);
+//        table.add(fte);
+//        return fte;
+//    }
 
     // receive a file table entry reference
     // save the corresponding inode to the disk
